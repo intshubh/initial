6fbf23ca278ded73c4db65e0f2b9fd43016b63b8
intshubh <shubham.bhatia@paytm.com>
2018-08-28 10:44:32 +0530

    hellos

--- script2.py
+++ script2.py
@@ -9,7 +9,7 @@ print(subprocess.call("git diff", shell=True))
 print "okay very gud"
 
 print(subprocess.call("git add .", shell=True))
-print(subprocess.call("git commit -m hello ", shell=True))
+print(subprocess.call("git commit -m hellos ", shell=True))
 print(subprocess.call("git push origin four ", shell=True))
 
 
